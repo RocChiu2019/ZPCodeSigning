932a4cd709595c3d1fc6c08e3460aabdba9e9783
赵鹏 <you@example.com>
2018-12-13 22:36:53 +0800

    创建项目。

---
+++ 代码签名/代码签名/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  代码签名
+//
+//  Created by 赵鹏 on 2018/12/12.
+//  Copyright © 2018 赵鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
